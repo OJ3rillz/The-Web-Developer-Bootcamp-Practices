ef3427144291bd4153a912660b3cd4e5f3e7aba8
TimmyJoseph <timitrilli@gmail.com>
2018-08-07 20:19:17 +0100

    annoyingOmatic code along

---
+++ annoying.js
@@ -0,0 +1,13 @@
+var answer = prompt("are we there yet?");
+
+
+while (answer !== "yes" && answer !== "yeah") {
+    var answer = prompt("tYPE IN THE OBVIOUS ANSWER YOU FOOL :)");
+}
+
+alert("YAY. YOU FINALLY GOT IT RIGHT")
+
+//if(answer === "yes") {
+ //   alert("YAY, We made it!")
+//else {
+}
