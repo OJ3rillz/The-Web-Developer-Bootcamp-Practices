48df7c3ca8f740d5eaf5895c47f60f61eb179325
TimmyJoseph <timitrilli@gmail.com>
2018-08-09 17:44:20 +0100

    forLoop Exercise Completed

--- forloops.js
+++ forloops.js
@@ -11,3 +11,20 @@ for(var i = 13; i <= 40; i += 1) {
         console.log(i);
     }
 }
+
+console.log("PRINTING ALL ODDS NUMBERS BETWEEN 300 AND 333")
+
+for (var i = 300; i <= 333; i +=1){
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+
+console.log("PRINTING ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50")
+
+for(var i = 5; i <= 50; i++){
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log(i);
+    }
+}
