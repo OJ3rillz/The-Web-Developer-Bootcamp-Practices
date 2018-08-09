3ce532efc0a265cc3e40c8c370f26c1aa88eb5a9
TimmyJoseph <timitrilli@gmail.com>
2018-08-09 17:23:08 +0100

    forLoops Progress

--- forloops.js
+++ forloops.js
@@ -1,1 +1,13 @@
-alert ("connected");
+console.log("PRINTING ALL NUMBERS BETWEEN -10 AND 20")
+
+for (var i = -10; i<=20; i++){
+    console.log(i)
+}
+
+console.log("PRINTING ALL EVEN NUMBERS BETWEEN 10 AND 40")
+
+for(var i = 13; i <= 40; i += 1) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
