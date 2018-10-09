98cdf6ecca3db8e0a4beeed86ba031081d732fa9
TimmyJoseph <timitrilli@gmail.com>
2018-10-09 11:11:37 +0100

    add "jQuery Exercise Completed"

--- jQueryExercise.js
+++ jQueryExercise.js
@@ -1,1 +1,8 @@
+//1
 $("div").css("background", "purple");
+//2
+$("div.highlight").css("width", "200px");
+//3
+$("third").css("border", "2px solid orange");
+//4
+$("div:first").css("color", "pink");
