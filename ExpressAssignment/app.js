6cec3cb3736284e90e47f5657d73ccea04fe56a5
TimmyJoseph <timitrilli@gmail.com>
2018-10-28 11:45:29 +0100

    Assignment completed

--- ExpressAssignment/app.js
+++ ExpressAssignment/app.js
@@ -20,9 +20,17 @@ app.get("/speak/:animal", function(req, res){
 
 app.get("/repeat/:message/:times", function(req, res){
       var message = req.params.message;
-      var times = req.params.times;
-      res.send("Message: " + message + "Times: " +times);
+      var times =Number(req.params.times);
+      var result = "";
+      for(var i = 0; i< times; i++){
+         result += message + " ";
+      }
+      res.send(result);
 });
+
+app.get("*", function(req,res){
+      res.send("Sorry page not found");
+})
 
 app.listen(3000, function(){
       console.log("Server has started");
