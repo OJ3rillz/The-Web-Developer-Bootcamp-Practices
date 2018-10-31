96faf5e759de2d1a4ef7abbc47a9cded4014a04a
TimmyJoseph <timitrilli@gmail.com>
2018-10-31 12:08:39 +0100

    add console.log for starting server

--- FirstExpressApp/PostReqDemo/app.js
+++ FirstExpressApp/PostReqDemo/app.js
@@ -1,1 +1,12 @@
-var 
+var express = required("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+
+});
+
+app.listen(3000, function(){
+      console.log("Server has started");
+  });
