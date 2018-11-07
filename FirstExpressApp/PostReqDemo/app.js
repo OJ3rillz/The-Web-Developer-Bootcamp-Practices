57023685682bc57f3e585c24a1e238e3a867365d
TimmyJoseph <timitrilli@gmail.com>
2018-11-07 12:08:39 +0100

    postRequest Code along

--- FirstExpressApp/PostReqDemo/app.js
+++ FirstExpressApp/PostReqDemo/app.js
@@ -1,10 +1,15 @@
-var express = required("express");
+var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
 res.render("home");
+});
+
+app.get("/friends", function(req, res){
+    var friends = ["Tony", ]
+res.render("friends");
 });
 
 app.listen(3000, function(){
