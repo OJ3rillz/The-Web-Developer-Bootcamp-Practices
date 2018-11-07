11f5def90490722686a486b4ecdf6cb16c34fe10
TimmyJoseph <timitrilli@gmail.com>
2018-11-07 18:11:50 +0100

    progress

--- FirstExpressApp/PostReqDemo/app.js
+++ FirstExpressApp/PostReqDemo/app.js
@@ -1,18 +1,24 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get("/", function(req, res){
 res.render("home");
 });
 
 app.post("/addfriend", function(req, res){
+    console.log (req.body.newfriend); 
     res.send("YOU HAVE REACHED THE POST ROUTE!!")
 });
 
 app.get("/friends", function(req, res){
-    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    
 res.render("friends", {friends: friends});
 });
 
