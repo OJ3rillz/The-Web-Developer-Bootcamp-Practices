7896a16565851b3806937d88e54e75c5c57f0df1
TimmyJoseph <timitrilli@gmail.com>
2018-12-19 21:42:11 +0100

    add campgrounds.ejs

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var app = express();
 
+
+app.set("view engine", "ejs")
+
+
 app.get("/", function(req, res){
-      res.send("This will be the landing page soon!")
+      res.render("landing")
 })
+
+app.get("/campgrounds", function(req, res){
+      var campgrounds = [
+            {name: "Salmon Creek", image: ""},
+            {name: "Granite hill", image: ""},
+            {name: "Mountain Goat's Rest", image: ""}
+      ]
+
+      res.render("campgrounds");
+
+});
 
 app.listen(3000, function(){
       console.log("Yelpcamp Server has started!");
