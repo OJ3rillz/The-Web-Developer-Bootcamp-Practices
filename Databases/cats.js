b66cfb0fcf0b3d71ee5e87ac0c1300d0ff5efc9d
TimmyJoseph <timitrilli@gmail.com>
2019-02-21 11:42:33 +0100

    add make new one to add in the database directly

--- Databases/cats.js
+++ Databases/cats.js
@@ -7,7 +7,32 @@ var catSchema = new mongoose.Schema({
       temperament: String
 });
 
-var Cat = mongoose.model("Cat", catSchema);
+  var Cat = mongoose.model("Cat", catSchema);
 // adding a new cat to the DB
+//var george = new Cat({
+//name: "Mrs Norris",
+//age: "7",
+//temperament: "Evil"
+//});
+
+//george.save(function(err, cat){
+  //   if(err){
+    //       console.log("SOMETHING WENT WRONG!")
+     //} else {
+       //    console.log("We just saved a cat to the db:")
+         //  console.log(cat);
+     //}
+//});
+Cat.create()
 
 //retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats){
+      if (err){
+            console.log("OH NO, ERROr!");
+            console.log(err);
+      } else {
+            console.log("ALL THE CATS.......");
+            console.log(cats);
+      }
+});
