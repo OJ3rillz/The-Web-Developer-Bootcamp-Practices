2b89fe136e8894b5d334c9aa030dbb645dbec60d
TimmyJoseph <timitrilli@gmail.com>
2019-02-21 19:07:53 +0100

    add schema setup and mongoose variable

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose")
 
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+
+// SCHEMA SETUP 
+var camproundSchema = new mongoose.Schema({
+      name:String,
+      image: String
+});
 
 var campgrounds = [
       {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f5c27ba1e4b1b1_340.jpg"},
