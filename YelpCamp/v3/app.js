cfc72e018fb4ad61615e207b14bf2bc3be2ad8a7
OJ3rillz <timitrilli@gmail.com>
2019-03-01 23:38:41 +0100

    clean app.js a little

--- YelpCamp/v3/app.js
+++ YelpCamp/v3/app.js
@@ -3,45 +3,34 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose")
 
-
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/yelp_campv3");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// SCHEMA SETUP 
-var camproundSchema = new mongoose.Schema({
-      name: String,
-      image: String
-});
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-      { 
-        name: "Salmon Creek", 
-        image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f5c27ba1e4b1b1_340.jpg"
-      }, function(err, campground){
-            if (err){
-                  console.log(err);
-            } else{
-                  console.log("NEWLY CREATED CAMPGROUND: ");
-                  console.log(campground);
-            }
-      });
-
-var campgrounds = [
-      {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f5c27ba1e4b1b1_340.jpg"},
-      {name: "Granite hill", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104491f5c27ba1e4b1b1_340.jpg"},
-      {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e835b20e29f7083ed1584d05fb1d4e97e07ee3d21cac104491f5c27ba1e4b1b1_340.jpg"}
-]
+//Campground.create(
+ //     { 
+  //      name: "Salmon Creek", 
+    //    image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f5c27ba1e4b1b1_340.jpg"
+      //}, function(err, campground){
+        //    if (err){
+          //        console.log(err);
+            //} else{
+              //    console.log("NEWLY CREATED CAMPGROUND: ");
+                //  console.log(campground);
+            //}
+     // });
 
 app.get("/", function(req, res){
       res.render("landing")
-})
+});
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-      
-
+    // Get all campgrounds from DB      
+    Campground.find({}, function(err))
       res.render("campgrounds",{campgrounds:campgrounds});
 
 });
