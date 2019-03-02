95d2c7167a21e32ec63d3ab052dcd858e2bd233e
OJ3rillz <timitrilli@gmail.com>
2019-03-02 22:36:35 +0100

    trial and error

--- YelpCamp/v3/models/campgrounds.js
+++
@@ -1,7 +0,0 @@
-var camproundSchema = new mongoose.Schema({
-      name: String,
-      image: String,
-      description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
