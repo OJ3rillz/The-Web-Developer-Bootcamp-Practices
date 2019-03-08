c172a9f979396a5ce4565d9de0a5680f7e69deb1
OJ3rillz <timitrilli@gmail.com>
2019-03-08 14:48:20 +0100

    add progress (login.ejs)

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -23,6 +23,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser()); 
 
@@ -67,6 +68,13 @@ app.post("/register", function(req, res){
 // render login form
 app.get("/login", function(req, res){
   res.render("login");
+});
+
+//login logic
+app.post("/login", passport.authenticate("local" ,{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
 });
 
 app.listen(3000, function(){
