20786158c77ca2b4ad801c1090b74f388f18b7ac
OJ3rillz <timitrilli@gmail.com>
2019-03-08 13:13:13 +0100

    add progress

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,4 +1,24 @@
 var express = require("express");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
+
+//ROUTES
+
+//==============
+
+app.get("/", function(req, res){
+      res.render("home");
+});
+
+app.get("/secret", function(req, res){
+    res.render("secret");
+});
+
+//Auth Routes
+
+app.listen(3000, function(){
+      console.log("App has started");
+  });
